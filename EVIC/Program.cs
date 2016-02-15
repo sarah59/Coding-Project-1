4d8c6ccd97929e88666bd2f3dab2fa60fc5913fb
ledevers <ledevers@ksu.edu>
2016-02-15 15:41:25 -0600

    here

--- EVIC/Program.cs
+++ EVIC/Program.cs
@@ -17,29 +17,30 @@ namespace EVIC
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-
-            if (Console.ReadKey().Key == ConsoleKey.UpArrow)
-            {
-                Console.WriteLine("1) System Status\n 2) Warning Message\n 3) Personal Setting\n 4) Temperature Display\n 5) Trip Info\n");
-                if(Console.ReadKey().Key == ConsoleKey.NumPad1)
+            while (true) {
+                if (Console.ReadKey().Key == ConsoleKey.UpArrow)
                 {
-                    System_Status _oilChange = new System_Status(); 
-                }
-                else if(Console.ReadKey().Key == ConsoleKey.NumPad2)
-                {
-                    Warning _warning = new Warning();
-                }
-                else if(Console.ReadKey().Key == ConsoleKey.NumPad3)
-                {
-                    Settings _units = new Settings();
-                }
-                else if(Console.ReadKey().Key == ConsoleKey.NumPad4)
-                {
-                    //Temperature _degree = new Temperature();
-                }
-                else if(Console.ReadKey().Key == ConsoleKey.NumPad5)
-                {
-                    Odometer _odometer = new Odometer();
+                    Console.WriteLine("1) System Status\n 2) Warning Message\n 3) Personal Setting\n 4) Temperature Display\n 5) Trip Info\n");
+                    if (Console.ReadKey().Key == ConsoleKey.NumPad1)
+                    {
+                        System_Status _oilChange = new System_Status();
+                    }
+                    else if (Console.ReadKey().Key == ConsoleKey.NumPad2)
+                    {
+                        Warning _warning = new Warning();
+                    }
+                    else if (Console.ReadKey().Key == ConsoleKey.NumPad3)
+                    {
+                        Settings _units = new Settings();
+                    }
+                    else if (Console.ReadKey().Key == ConsoleKey.NumPad4)
+                    {
+                        //Temperature _degree = new Temperature();
+                    }
+                    else if (Console.ReadKey().Key == ConsoleKey.NumPad5)
+                    {
+                        Odometer _odometer = new Odometer();
+                    }
                 }
 
                 
