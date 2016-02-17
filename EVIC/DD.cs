e9865e96157234c7b4005a04685250a2921fa828
Sarah Martin <sarahjoy.martin@sbcglobal.net>
2016-02-17 09:07:56 -0600

    this is coding project 1 - rough draft

---
+++ EVIC/DD.cs
@@ -0,0 +1,232 @@
+﻿/*
+*Written By: Leah Dever and Sarah Martin
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVIC
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Odometer
+    {
+
+        int ode;
+        int _tripA;
+        int _tripB;
+
+        public Odometer()
+        {
+            this.ode = 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void increment()
+        {
+            ode++;
+            _tripA++;
+            _tripB++;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int currentMileage()
+        {
+            return ode;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class TripInfo
+    {
+        int _tripA;
+        int _tripB;
+        Odometer _odometer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_odometer"></param>
+        public TripInfo(Odometer _odometer)
+        {
+            this._odometer = _odometer;
+            this._tripA = _odometer.currentMileage();
+            this._tripB = _odometer.currentMileage();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int getTripAValue()
+        {
+            return _odometer.currentMileage() - _tripA;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int getTripBValue()
+        {
+            return _odometer.currentMileage() - _tripB;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void resetA()
+        {
+            _tripA = _odometer.currentMileage();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void resetB()
+        {
+            _tripB = _odometer.currentMileage();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class System_Status
+    {
+        int lastOilChange;
+        Odometer _odometer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_odometer"></param>
+        public System_Status(Odometer _odometer)
+        {
+            this._odometer = _odometer;
+            lastOilChange = _odometer.currentMileage();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int Oil()
+        {
+            int change = 3000 - (_odometer.currentMileage() - lastOilChange);
+            return change;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int ResetOil()
+        {
+            lastOilChange = _odometer.currentMileage();
+            return lastOilChange;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Temperature
+    {
+        int inside;
+        int outside;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public Temperature(int a, int b)
+        {
+            inside = a;
+            outside = b;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int TempOutside()
+        {
+            return outside;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int TempInside()
+        {
+            return inside;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Settings
+    {
+        public bool usingMetric;
+        public Settings()
+        {
+            usingMetric = false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Change()
+        {
+            usingMetric = !usingMetric;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Warnings
+    {
+        bool engine;
+        bool ajar;
+        bool changeOil;
+
+        public Warnings()
+        {
+            this.engine = true;
+            this.ajar = true;
+            this.changeOil = true;
+        }
+
+        public bool Engine()
+        {
+            return engine;
+        }
+
+        public bool Ajar()
+        {
+            return ajar;
+        }
+
+        public bool ChangeOil()
+        {
+            return changeOil;
+        }
+    }
+}
+
--- EVIC/Program.cs
+++ EVIC/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿/*
+*Written By: Leah Devers and Sarah Martin
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace EVIC
 {
@@ -11,72 +15,324 @@ namespace EVIC
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            while (true) {
-                if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+            Odometer _odometer = new Odometer();
+            TripInfo trip = new TripInfo(_odometer);
+            Settings _settings = new Settings();
+            System_Status _ss = new System_Status(_odometer);
+            Temperature _t = new Temperature(0, 0);
+            Warnings _messages = new Warnings();
+
+            ConsoleKey _keyPress;
+            int _aOrB = 0;
+            int _howManyMiles = 0;
+            int _inOrOut = 0;
+            bool _ajar = _messages.Ajar();
+            bool _engine = _messages.Engine();
+            bool _changeOil = _messages.ChangeOil();
+
+            while (true)
+            {
+
+                _keyPress = Console.ReadKey().Key;
+
+                int censor = 0;
+
+                if (_keyPress == ConsoleKey.RightArrow)
                 {
-                    Console.WriteLine("1) System Status\n 2) Warning Message\n 3) Personal Setting\n 4) Temperature Display\n 5) Trip Info\n");
-                    if (Console.ReadKey().Key == ConsoleKey.NumPad1)
+                    censor++;
+                }
+                else if(_keyPress == ConsoleKey.LeftArrow)
+                {
+                    censor--;
+                }
+
+                if (censor == 0)
+                {
+                    if (_keyPress == ConsoleKey.UpArrow || _keyPress == ConsoleKey.DownArrow)
                     {
-                        System_Status _oilChange = new System_Status();
+                        if (_howManyMiles == 0)
+                        {
+                            _howManyMiles = 1;
+
+                            if (_settings.usingMetric == true)
+                            {
+                                int x = ToMetric(_ss.Oil());
+                                Display(x.ToString());
+                            }
+                            else
+                            {
+                                Display(_ss.Oil().ToString());
+                            }
+                        }
+                        else
+                        {
+                            _howManyMiles = 0;
+                            if (_settings.usingMetric == true)
+                            {
+                                int y = ToMetric(_odometer.currentMileage());
+                                Display(y.ToString());
+                            }
+                            else
+                            {
+                                Display(_odometer.currentMileage().ToString());
+                            }
+                        }
                     }
-                    else if (Console.ReadKey().Key == ConsoleKey.NumPad2)
+
+                    else if (_keyPress == ConsoleKey.Spacebar)
                     {
-                        Warning _warning = new Warning();
-                    }
-                    else if (Console.ReadKey().Key == ConsoleKey.NumPad3)
-                    {
-                        Settings _units = new Settings();
-                    }
-                    else if (Console.ReadKey().Key == ConsoleKey.NumPad4)
-                    {
-                        //Temperature _degree = new Temperature();
-                    }
-                    else if (Console.ReadKey().Key == ConsoleKey.NumPad5)
-                    {
-                        Odometer _odometer = new Odometer();
+                        if (_howManyMiles == 1)
+                        {
+                            if (_settings.usingMetric == true)
+                            {
+                                int y = ToMetric(_ss.ResetOil());
+                                Display(y.ToString());
+                            }
+                            else
+                            {
+                                Display(_ss.ResetOil().ToString());
+                            }
+                        }
                     }
                 }
 
-                
-            }
-            
-
-        }
-
-        public int temp()
-        {
-            try
-            {
-                Console.WriteLine("Please enter the outside temperature: ");
-                int outside = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Please enter the inside temperature of the vehicle: ");
-                int inside = Convert.ToInt32(Console.ReadLine());
-
-                Temperature _change = new Temperature(outside, inside);
-
-                while (Console.ReadKey().Key == ConsoleKey.UpArrow)
+                if (censor == 1 || censor == -4)
                 {
-                    if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+                    if (_ajar == false && _engine == false && _changeOil == false)
                     {
-                        Console.WriteLine(_change.TempOutside());
+                        Display("No Warning Messages");
                     }
+                    
                     else
                     {
-                        Console.WriteLine(_change.TempInside());
+                        while (_keyPress == ConsoleKey.UpArrow)
+                        {
+                            if(_ajar == true && _keyPress == ConsoleKey.UpArrow)
+                            {
+                                Display("Door Ajar");
+                            }
+                            else if(_engine == true && _keyPress == ConsoleKey.UpArrow)
+                            {
+                                Display("Check Engine Soon");
+                            }
+                            else if(_changeOil == true && _keyPress == ConsoleKey.UpArrow)
+                            {
+                                Display("Oil Change");
+                            }
+                        }
+                        
+                    }
+                }
+
+                else if (censor == 2 || censor == -3)
+                {
+                    if (_keyPress == ConsoleKey.UpArrow || _keyPress == ConsoleKey.DownArrow || _keyPress == ConsoleKey.Spacebar)
+                    {
+                        _settings.Change();
+                    }
+                }
+                else if (censor == 3 || censor == -2)
+                {
+                    if (_keyPress == ConsoleKey.UpArrow || _keyPress == ConsoleKey.DownArrow)
+                    {
+                        if (_inOrOut == 0)
+                        {
+                            _inOrOut = 1;
+                            if (_settings.usingMetric == true)
+                            {
+                                int x = ToCelcius(_t.TempInside());
+                                Display(x.ToString());
+                            }
+                            else
+                            {
+                                Display(_t.TempInside().ToString());
+                            }
+                        }
+                        else
+                        {
+                            _inOrOut = 0;
+                            if (_settings.usingMetric == true)
+                            {
+                                int y = ToCelcius(_t.TempOutside());
+                                Display(y.ToString());
+                            }
+                            else
+                            {
+                                Display(_t.TempOutside().ToString());
+                            }
+                        }
+                    }
+                }
+
+                else if (censor == 4 || censor == -1)
+                {
+                    if (_keyPress == ConsoleKey.UpArrow || _keyPress == ConsoleKey.DownArrow)
+                    {
+                        if (_aOrB == 0)
+                        {
+                            _aOrB = 1;
+                            if (_settings.usingMetric == true)
+                            {
+                                int x = ToMetric(trip.getTripAValue());
+                                Display(x.ToString());
+                            }
+                            else
+                            {
+                                Display(trip.getTripAValue().ToString());
+                            }
+                        }
+                        else
+                        {
+                            _aOrB = 0;
+                            if (_settings.usingMetric == true)
+                            {
+                                int y = ToMetric(trip.getTripBValue());
+                                Display(y.ToString());
+                            }
+                            else
+                            {
+                                Display(trip.getTripBValue().ToString());
+                            }
+                        }
+                    }
+
+                    else if (_keyPress == ConsoleKey.Spacebar)
+                    {
+                        if (_aOrB == 0)
+                        {
+                            trip.resetA();
+                        }
+                        else
+                        {
+                            trip.resetB();
+                        }
+                    }
+                    else if(_keyPress == ConsoleKey.Enter)
+                    {
+                        _odometer.increment();
+                    }
+                }
+
+                else if(censor > 4 || censor < -4)
+                {
+                    censor = 4;
+                }
+                
+                else
+                {
+                    censor = 0;
+                }
+
+                if(_keyPress == ConsoleKey.NumPad1)
+                {
+                    status(_odometer);
+                }
+                else if(_keyPress == ConsoleKey.NumPad2)
+                {
+                    try
+                    {
+                        Console.WriteLine("A) Door Ajar \n B) Check Engine Soon \n C) Oil Change");
+                        Console.WriteLine("Press the corresponding letter to turn the warning message on/off");
+                        if (_keyPress == ConsoleKey.A)
+                        {
+                            toggle(_ajar);
+                        }
+                        else if (_keyPress == ConsoleKey.B)
+                        {
+                            toggle(_engine);
+                        }
+                        else if (_keyPress == ConsoleKey.C)
+                        {
+                            toggle(_changeOil);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Must choose one of the three letters.");
+                    }
+                }
+                else if(_keyPress == ConsoleKey.NumPad3)
+                {
+                    int valueA = 0;
+                    int valueB = 0;
+
+                    Console.WriteLine("A) Outside Temperature \n B) Inside Temperature");
+
+                    if (Console.ReadKey().Key == ConsoleKey.A)
+                    {
+                        Console.WriteLine("Please enter the outside temperature: ");
+                        valueA = temp();
+                    }
+                    if (Console.ReadKey().Key == ConsoleKey.B)
+                    {
+                        Console.WriteLine("Please enter the inside temperature of the vehicle: ");
+                        valueB = temp();
                     }
                 }
             }
-            catch (Exception ex)
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="miles"></param>
+        /// <returns></returns>
+        public static int ToMetric(int miles)
+        {
+            return Convert.ToInt32(1.609 * miles);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        public static void Display(string s)
+        {
+            Console.WriteLine(string.Format("[{0}]"), s);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="temp"></param>
+        /// <returns></returns>
+        public static int ToCelcius(int temp)
+        {
+            return Convert.ToInt32((temp - 32) / 1.8);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static int temp()
+        {
+            int degree;
+            return degree = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void status(Odometer ode)
+        {
+            Odometer o = ode;
+            try
             {
-                Console.WriteLine("Please enter a valid number for the temperature");
+                Console.WriteLine("Press enter to increment the mileage by 1 mile.");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    o.increment();
+                }
             }
-            
+            catch (Exception)
+            {
+                Console.Write("Press Enter to increment.");
+            }
+        }
+
+        public static bool toggle(bool w)
+        {
+            return !w;
+        }
     }
 }
